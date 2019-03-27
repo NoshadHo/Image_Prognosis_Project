4df2970e87db57a8e8477f692fd5461e20e4b12e
Hosseini <noshadh@umhs.med.umich.edu>
2019-03-27 11:36:37 -0400

    Adding new file

--- Clustring_tiles.R
+++ Clustring_tiles.R
@@ -16,7 +16,6 @@ data_tile = data_tile %>% select(-ImageNumber,
                                    -AreaShape_EulerNumber,
                                    -Number_Object_Number)
 
-
 #save the colnames
 col_names = colnames(data_tile)
 
