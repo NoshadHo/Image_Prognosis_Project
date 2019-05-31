94d6c7d99ba117c1e8270ae337b0bd7d52d379e8
noshadho <noshadho@gmail.com>
2019-05-31 14:51:48 -0400

    initiate the coding file

---
+++ one_hot_encoding_clinData.R
@@ -0,0 +1,6 @@
+#############################################################################################################################
+## Author: Noshad Hosseini
+## Start Date: 5-31-2019
+## End Date:
+## Subject: Converting categorical clinical data (TCGA Patients) into usable data by nueral nets (using one hot encoding)
+#############################################################################################################################
