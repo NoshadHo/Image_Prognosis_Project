d37867d64f73b8552be8bdc4c4045d0d5e6ff1ab
noshadho <noshadho@gmail.com>
2019-05-31 15:23:58 -0400

    reading and one hot encoding part added, removing redundant columns left

--- one_hot_encoding_clinData.R
+++ one_hot_encoding_clinData.R
@@ -4,3 +4,25 @@
 ## End Date:
 ## Subject: Converting categorical clinical data (TCGA Patients) into usable data by nueral nets (using one hot encoding)
 #############################################################################################################################
+
+#here we assume we have the data output from PostProcessing.R
+#we are only going to use final_data_file image Id's
+
+#libraries:
+library(tidyverse)
+library(mlr)
+library(rlang)
+#read the clinical data file:
+clinical = readxl::read_xlsx("/scratch/lgarmire_fluxm/noshadh/Top_dense_10_Stanford_method/mmc1 (1).xlsx",
+                             col_names = TRUE)
+
+#first remove NA and Not-applicable columns
+clinical = clinical %>% select(-!!1) #remove first column
+#INCOMPLETE*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+
+#do the one hot encoding
+clinical_OHE = clinical %>% mutate_if(is.character,as.factor) %>% 
+  createDummyFeatures(method = 'refrence')
+
+#write
