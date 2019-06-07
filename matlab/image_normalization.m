7fc8dc459d48245b04d57c76ee38565113104cc2
noshadho <noshadho@gmail.com>
2019-06-07 16:41:26 -0400

    first try on normalization->failed

--- matlab/image_normalization.m
+++ matlab/image_normalization.m
@@ -31,7 +31,38 @@
     tic             %to measure how long does it take to load the image
     image = imread(filename);
     toc
+    image_red =     image(:,:,1);
+    image_green =   image(:,:,2);
+    image_blue =    image(:,:,3);
+    
+    %Look at the histogram of these channels:
+    h = histogram(image_red_2)
 %% Normalize one sample -> extend it to all samples
+    %read the second image to normalize:
+    fileName = dirname(2);  %we use first file as the refrence file
+    %set the image path
+    scratch = strcat('/scratch/lgarmire_fluxm/noshadh/Diagnostic_Slide_images/', fileName, '/');
+    cd(string(scratch));
+    svsFile = dir('*.svs');
+    filename = svsFile(1).name;
+    tic             %to measure how long does it take to load the image
+    image_2 = imread(filename);
+    toc
+    image_red_2 =     image_2(:,:,1);
+    image_green_2 =   image_2(:,:,2);
+    image_blue_2 =    image_2(:,:,3);
 
+    %make them to have a same mean
+    image_red_2_temp = image_red_2 - mean(mean(image_red_2)) + mean(mean(image_red));
+    image_green_2_temp = image_green_2 - mean(mean(image_green_2)) + mean(mean(image_green));
+    image_blue_2_temp = image_blue_2 - mean(mean(image_blue_2)) + mean(mean(image_blue));
+    %any value less than 0 should be zero
+    image_red_2_temp(image_red_2_temp < 0) = 0;
+    image_green_2_temp(image_green_2_temp < 0) = 0;
+    image_blue_2_temp(image_blue_2_temp < 0) = 0;
+    %now make the image
+    image_2_temp(:,:,1) = image_red_2_temp;
+    image_2_temp(:,:,2) = image_green_2_temp;
+    image_2_temp(:,:,3) = image_blue_2_temp
 %% Save the normalized images
 %% Plot distribution of colors for each channel, after and befor normalization
